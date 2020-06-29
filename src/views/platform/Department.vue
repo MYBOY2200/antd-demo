0805f6d22850c95901e749186bff9578c65ef174
MYBOY <569964031@qq.com>
2020-06-29 18:10:42 +0800

    departmentMngm

--- src/views/platform/Department.vue
+++ src/views/platform/Department.vue
@@ -7,24 +7,13 @@
  -->
 <template>
   <div>
-    <div>
-      <a-card title="Default size card" style="width: 300px">
-        <a slot="extra" href="#">more</a>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-      </a-card>
-      <br />
-      <a-card size="small" title="Small size card" style="width: 300px">
-        <a slot="extra" href="#">more</a>
-        <p>card content</p>
-        <p>card content</p>
-        <p>card content</p>
-      </a-card>
-    </div>
+    <div class="department-wrapper"></div>
   </div>
 </template>
 
 <style lang="less" scoped>
+.department-wrapper {
+  padding: 24px;
+  background-color: #fff;
+}
 </style>
-
